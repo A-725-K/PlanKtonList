bca872ad68cdf22fceeb61d32f24e31b54fd8db5
kennedy12 <kennedy12@a725k>
2018-04-19 02:33:17 +0200

    Added useless comment to MainPLK

--- src/main_and_test/MainPLK.java
+++ src/main_and_test/MainPLK.java
@@ -10,6 +10,7 @@ import gui.Program_Gui;
 
 public class MainPLK {
 	public static void main(String[] args) {
+		//Start the program and the GUI
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
